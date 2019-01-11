4514430efec9053bead33c813356dbc60b4761d1
jsp1256 <googg@jsp1256.uu.me>
2019-01-11 14:11:46 +0800

    规范文件格式，对部分不规则的名称进行修改

--- yande.py
+++ yande.py
@@ -1,25 +1,29 @@
-import os#引入文件模块
-import re#正则表达式
+import os  # 引入文件模块
+import re  # 正则表达式
 import urllib.request
 
-#连接网页并返回源码
+
+# 连接网页并返回源码
 def open_url(url):
-      try:
-            req = urllib.request.Request(url)
-            req.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
-            response = urllib.request.urlopen(req)
-            status_code = response.code
-            html = response.read()
-            return html
-      except:
-            print(url + " 404")
-            return 404
+    try:
+        req = urllib.request.Request(url)
+        req.add_header("User-Agent",
+                       "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/55.0.2883.87 Safari/537.36")
+        response = urllib.request.urlopen(req)
+        status_code = response.code
+        html = response.read()
+        return html
+    except Exception as e:
+        print(e, url + " 404")
+        return 404
+
 
 def mkdir(path):
-    '''
+    """
     :param path: 路径
     :return:
-    '''
+    """
     # 引入模块
     import os
 
@@ -30,10 +34,10 @@ def mkdir(path):
     # 判断路径是否存在
     # 存在     True
     # 不存在   False
-    isExists = os.path.exists(path)
+    is_exists = os.path.exists(path)
 
     # 判断结果
-    if not isExists:
+    if not is_exists:
         # 如果不存在则创建目录
         # 创建目录操作函数
         os.makedirs(path)
@@ -45,35 +49,35 @@ def mkdir(path):
         return False
 
 
-def Yande1():
+def yande1():
     pages1 = int(input('请输入你要下载的起始页面数：'))
     pages2 = int(input('请输入你要下载的末尾页面数：'))
     mkdir('img')
-    for i in range(pages1,pages2+1):
+    for i in range(pages1, pages2 + 1):
         imgs = 1
         print(i)
-        url = 'https://yande.re/post?page='+str(i)
-        #print(url)
-        floder = "E:\Python\爬虫\yande\img\page"+str(i)
+        url = 'https://yande.re/post?page=' + str(i)
+        # print(url)
+        floder = "E:\Python\爬虫\yande\img\page" + str(i)
         mkdir(floder)
         os.chdir(floder)
 
         html = open_url(url)
         html = html.decode('gbk', 'ignore')
-        #print(html)
+        # print(html)
         img_adds = []
         img_adds = re.findall(r'<a class="directlink largeimg" href="([^"]+\.jpg)"', html)
         for i in img_adds:
-            filename = str(imgs)+'.jpg'
+            filename = str(imgs) + '.jpg'
             imgs += 1
             img_html = open_url(i)
             if img_html == 404:
                 continue
             with open(filename, 'wb') as f:
                 f.write(img_html)
-                print(i+'下载完成....')
-        #print(len(img_adds))
+                print(i + '下载完成....')
+        # print(len(img_adds))
 
 
 if __name__ == '__main__':
-    Yande1()
+    yande1()
